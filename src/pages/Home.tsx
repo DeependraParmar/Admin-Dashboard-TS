53538fd74c8dc11f9fc69d1724aa971efa196624
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-01-29 14:58:18 +0530

    Added Home.jsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+        <Link to="/admin/dashboard">Go to Dashboard</Link>
+    </div>
+  )
+}
+
+export default Home
